9374dad46d92e9833f5fefcb5ee556e82ca03912
coelhocarlos <carloscoelho_@msn.com>
2016-09-01 12:25:56 -0300

    Create insert.php

---
+++ insert.php
@@ -0,0 +1,21 @@
+<?php
+include 'db_conn.php';
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "INSERT INTO events (title, color, start,end)
+VALUES ('test', '#c0392b', '2016-09-01 02:00:00','2016-09-01 02:00:00')";
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
