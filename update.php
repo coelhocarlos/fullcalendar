a008282b63af9cdb13028f2020042a3faf19cb10
coelhocarlos <carloscoelho_@msn.com>
2016-09-01 12:36:15 -0300

    Create update.php

---
+++ update.php
@@ -0,0 +1,20 @@
+<?php
+include 'db_conn.php';
+
+// Create connection
+$conn = new mysqli('localhost', 'root', '', 'mydb');
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "UPDATE events SET lastname='Doe' WHERE id=2";
+
+if (mysqli_query($conn, $sql)) {
+    echo "Record updated successfully";
+} else {
+    echo "Error updating record: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
