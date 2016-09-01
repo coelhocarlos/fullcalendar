b77ca6707f31ceb7fbce00faf69e6c8936bc508d
coelhocarlos <carloscoelho_@msn.com>
2016-09-01 12:27:25 -0300

    Update insert.php

--- insert.php
+++ insert.php
@@ -2,12 +2,11 @@
 include 'db_conn.php';
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$mysqli = new mysqli('localhost', 'root', '', 'mydb');
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
 $sql = "INSERT INTO events (title, color, start,end)
 VALUES ('test', '#c0392b', '2016-09-01 02:00:00','2016-09-01 02:00:00')";
 
